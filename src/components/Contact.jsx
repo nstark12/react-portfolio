f42b65cd3d88ae95dccad57414b6b752355bb356
Marketing Director <nicolestark1103@gmail.com>
2023-10-20 15:18:23 -0500

    contact page done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,39 @@
+import { useState, useEffect } from 'react'
+import Form from './Form.jsx'
+
 const Contact = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const handleInputChange = e => {
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        // reset inputs
+        setFormState({
+            name: '',
+            email: '',
+            message: ''
+        })
+    }
     return (
         <>
             <div className="container contact my-5">
                 <h1>Contact Me</h1>
+                <Form 
+                    formState={formState}
+                    handleInputChange={handleInputChange}
+                    handleSubmit={handleSubmit}
+                />
                 
             </div>
         </>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,57 @@
-const Form = () => {
-
-}
-
+    const Form = props => {
+        return (
+          <form onSubmit={props.handleSubmit}>
+            <div className="mb-3">
+              <label
+                htmlFor="exampleInputEmail1"
+                className="form-label"
+              >
+                Name
+              </label>
+              <input
+                name="name"
+                value={props.formState.name}
+                onChange={props.handleInputChange}
+                type="text"
+                className="form-control"
+                required 
+              />
+            </div>
+            <div className="mb-3">
+              <label
+                htmlFor="exampleInputEmail1"
+                className="form-label"
+              >
+                Email address
+              </label>
+              <input
+                name="email"
+                value={props.formState.email}
+                onChange={props.handleInputChange}
+                type="email"
+                className="form-control"
+                required 
+              />
+            </div>
+            <div className="mb-3">
+              <label
+                htmlFor="exampleInputPassword1"
+                className="form-label"
+              >
+                Message
+              </label>
+              <textarea
+                name="message"
+                value={props.formState.message}
+                onChange={props.handleInputChange}
+                type="text"
+                className="form-control"
+                required 
+              />
+            </div>
+            <button type="submit" className="btn">Submit</button>
+          </form>
+        )
+      }
+      
 export default Form
